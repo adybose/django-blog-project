187a0eb437e13c319e41a866308bccd23c506351
Aditya Bose <13054902+adbose@users.noreply.github.com>
2020-11-01 22:48:28 +0530

    Add contact and about

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.contact_us, name='contact'),
     path('<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
 ]
--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Article
+from django.http import HttpResponse
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 class ArticleListView(ListView):
@@ -11,3 +14,21 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'detail.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+def contact_us(request):
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # send email code goes here
+            sender_name = form.cleaned_data['name']
+            sender_email = form.cleaned_data['email']
+            message = "{0} has sent you a new message:\n\n{1}".format(sender_name, form.cleaned_data['message'])
+            send_mail('New Enquiry', message, sender_email, ['enquiry@exampleco.com'])
+
+            return HttpResponse('Thank you for contacting us')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
